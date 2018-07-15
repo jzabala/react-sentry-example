99d33e46cbbb221559a3024d91f4a7e57943df7e
Johnny Zabala <jzabala.s@gmail.com>
2018-07-15 16:46:13 -0400

    feat(form): add form with error on submit

--- src/App.js
+++ src/App.js
@@ -3,16 +3,32 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { value: '' };
+  }
+  handleChange = event => {
+    this.setState({ value: event.target.value });
+  };
+  handleSubmit = event => {
+    event.preventDefault();
+    const { value } = this.state;
+    if (value) {
+      throw new Error(`Test error: ${value}`);
+    }
+  };
   render() {
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <h1 className="App-title">React + Sentry example app</h1>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <p>Type some text and submit to throw an error</p>
+        <form onSubmit={this.handleSubmit}>
+          <input onChange={this.handleChange} type="text" name="name" />
+          <input type="submit" value="Submit" />
+        </form>
       </div>
     );
   }
